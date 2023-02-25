4cd64bee2f4c50b27641965beec183b0b57fec46
Lanzerth <rvga1311@gmail.com>
2023-02-24 22:23:48 -0600

    Register

--- Frontend/src/app/pages/register/register.component.ts
+++ Frontend/src/app/pages/register/register.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class RegisterComponent {
+  constructor() {
+    this.currentUser = null;
+    this.errorMsg = null;
   }
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
 
+  currentUser: null;
+
+  errorMsg: string | null;
+
+  loginUser = () => {
+    // let newErrorMsg: string | null = null;
+    const username = this.loginForm.get('username')?.value;
+    const password = this.loginForm.get('password')?.value;
+    if (!username || username.length === 0) {
+      this.errorMsg = 'Username is required';
+      return;
+    }
+    if (!password || password.length === 0) {
+      this.errorMsg = 'Password is required';
+      return;
+    }
+
+  };
 }

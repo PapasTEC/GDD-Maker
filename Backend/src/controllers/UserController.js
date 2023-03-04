898c97d8a6700821439da9d96437f7b2822ff680
Lanzerth <rvga1311@gmail.com>
2023-03-04 17:04:49 -0600

    GDDM-2 Sign in

--- Backend/src/controllers/UserController.js
+++ Backend/src/controllers/UserController.js
@@ -1,6 +1,6 @@
 const userController = {};
 const Users = require("../models/UserModel");
-const { generatePasswordCode, sendPasswordCode } = require("../functions/utils");
+const { generatePasswordCode, sendEmail } = require("../functions/utils");
 
 userController.getUsers = async (req, res) => {
     const users = await Users.find();
@@ -10,32 +10,21 @@ userController.getUsers = async (req, res) => {
 userController.createUser = async (req, res) => {
     try {
         const { name, email  } = req.body;
-        const password = generatePasswordCode(10);
         const newUser = new Users({
             name,
             email,
-            password,
+            password:"_",
             owned_documents: [],
             shared_with_me_documents: [],
         });
         console.log("Inserting user: " + newUser);
-        await sendPasswordCode(email, password);
+        await sendEmail(email, "", "welcome");
         await newUser.save();
         res.status(200).json({ message: "User created" });
     } catch (error) {
         res.status(500).json({ message: error });
     }
 };
-
-function validateEmail(email) {
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-}
-
-function validatePassword(password) {
-    var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
-    return re.test(password);
-}
 
 userController.getUser = async (req, res) => {
     try {
@@ -78,5 +67,54 @@ userController.deleteUser = async (req, res) => {
         res.status(500).json({ message: error });
     }
 };
+
+userController.sendCodeUser = async (req, res) => {
+    try {
+        let user = await Users.findOne({ email: req.params.email });
+
+        if (!user) return res.status(404).json({ message: "User not found" });
+
+        const password = generatePasswordCode(10);
+        await sendEmail(user.email, password, "reset");
+        
+        const newUser = { password };
+
+        await user.updateOne(newUser);
+
+        res.status(200).json({ password });
+    } catch (error) {
+        res.status(500).json({ message: error });
+    }
+};
+
+userController.loginUser = async (req, res) => {
+    try {
+        const user = await Users.findOne({ email: req.params.email, password: req.params.password });
+
+        if (!user) return res.status(200).json(false);
+        res.status(200).json(true);
+
+        try {
+            let user = await Users.findOne({ email: req.params.email });
+    
+            if (!user) return res.status(404).json({ message: "User not found" });
+    
+            const password = "_";
+            
+            const newUser = { password };
+    
+            await user.updateOne(newUser);
+    
+            //res.status(200).json({ message: "Password updated" });
+        } catch (error) {
+            res.status(500).json({ message: error });
+        }
+
+    } catch (error) {
+        res.status(500).json({ message: error });
+    }
+};
+
+
 
 module.exports = userController;
--- Backend/src/routes/UserRoutes.js
+++ Backend/src/routes/UserRoutes.js
@@ -7,5 +7,7 @@ router.post('/add', userController.createUser);
 router.get('/get/:email', userController.getUser);
 router.put('/update/:email', userController.updateUser);
 router.delete('/delete/:email', userController.deleteUser);
+router.put('/sendCode/:email', userController.sendCodeUser);
+router.get('/login/:email/:password', userController.loginUser);
 
 module.exports = router;

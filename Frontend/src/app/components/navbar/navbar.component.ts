6c00158364c0aa3619d6f0825210989d5f70f73c
Abiel Porras Garro <66501176+juuzou13@users.noreply.github.com>
2023-03-06 16:23:41 -0600

    Union login-dashboard

--- Frontend/src/app/components/navbar/navbar.component.ts
+++ Frontend/src/app/components/navbar/navbar.component.ts
@@ -12,25 +12,31 @@ export class NavbarComponent implements OnInit {
   public focus;
   public listTitles: any[];
   public location: Location;
+
+  profileInfo: any;
+
   constructor(location: Location,  private element: ElementRef, private router: Router) {
     this.location = location;
   }
 
   ngOnInit() {
     this.listTitles = ROUTES.filter(listTitle => listTitle);
+    this.profileInfo = JSON.parse(localStorage.getItem('currentUser'));
   }
-  getTitle(){
-    var titlee = this.location.prepareExternalUrl(this.location.path());
-    if(titlee.charAt(0) === '#'){
-        titlee = titlee.slice( 1 );
-    }
 
-    for(var item = 0; item < this.listTitles.length; item++){
-        if(this.listTitles[item].path === titlee){
-            return this.listTitles[item].title;
-        }
-    }
-    return 'Dashboard';
-  }
 
 }
+
+  // getTitle(){
+  //   var titlee = this.location.prepareExternalUrl(this.location.path());
+  //   if(titlee.charAt(0) === '#'){
+  //       titlee = titlee.slice( 1 );
+  //   }
+
+  //   for(var item = 0; item < this.listTitles.length; item++){
+  //       if(this.listTitles[item].path === titlee){
+  //           return this.listTitles[item].title;
+  //       }
+  //   }
+  //   return 'Dashboard';
+  // }

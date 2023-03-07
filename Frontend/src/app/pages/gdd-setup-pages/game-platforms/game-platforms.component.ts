e46822fbea466b3d71d988b6d78ae41cebbb60aa
delgado7 <fabridelgado02@gmail.com>
2023-03-06 23:20:49 -0600

    Avance plataformas

--- Frontend/src/app/pages/gdd-setup-pages/game-platforms/game-platforms.component.ts
+++ Frontend/src/app/pages/gdd-setup-pages/game-platforms/game-platforms.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-game-platforms',
   templateUrl: './game-platforms.component.html',
-  styleUrls: ['./game-platforms.component.scss']
+  styleUrls: ['./game-platforms.component.scss', '../setupStyles.scss']
 })
 export class GamePlatformsComponent {
 

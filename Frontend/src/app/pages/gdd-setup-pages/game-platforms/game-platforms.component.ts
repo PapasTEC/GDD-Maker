d3e5769db94e3c97b8766e84c0c2e831c3f0d25d
delgado7 <fabridelgado02@gmail.com>
2023-03-08 01:11:08 -0600

    Plataformas listas

--- Frontend/src/app/pages/gdd-setup-pages/game-platforms/game-platforms.component.ts
+++ Frontend/src/app/pages/gdd-setup-pages/game-platforms/game-platforms.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class GamePlatformsComponent {
 
+  platforms = [
+    {"name":"Playstation", "image":"../../assets/img/platformIcons/playstation.png"}, 
+    {"name":"Xbox", "image":"../../assets/img/platformIcons/xbox.png"}, 
+    {"name":"Windows", "image":"../../assets/img/platformIcons/windows.png"}, 
+    {"name":"Nintendo Switch", "image":"../../assets/img/platformIcons/switch.png"},
+    {"name":"Linux", "image":"../../assets/img/platformIcons/linux.png"},
+    {"name":"MacOS", "image":"../../assets/img/platformIcons/mac.png"},
+    {"name":"Android", "image":"../../assets/img/platformIcons/android.png"},
+    {"name":"iOS", "image":"../../assets/img/platformIcons/ios.png"},
+    {"name":"Web", "image":"../../assets/img/platformIcons/web.png"},];
+
+  //create onginit function to get platforms from database
+  chosenPlatforms = [];
+  //create function to add platform to list
+  
+  public addOrRemove(platformID:number){
+    console.log("Platform ID: " + platformID);
+    if(this.chosenPlatforms.includes(platformID)){
+      this.chosenPlatforms.splice(this.chosenPlatforms.indexOf(platformID), 1);
+    }else{
+      this.chosenPlatforms.push(platformID);
+    }
+    this.updatePlatformIndicator();
+    console.log(this.chosenPlatforms);
+  }
+
+  updatePlatformIndicator(){
+    var platformsInDocToActivate = document.getElementsByClassName("overlayImage");
+    for(var i = 0; i < platformsInDocToActivate.length; i++){
+      if(this.chosenPlatforms.includes(i)){
+        platformsInDocToActivate[i].setAttribute("style", "display: block;");
+      }else{
+        platformsInDocToActivate[i].setAttribute("style", "display: none;");
+      }
+    
+      }
+  }
+
+  
+  ngOnInit(){
+    
+  }
+
 }

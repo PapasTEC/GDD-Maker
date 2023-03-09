81f87f07707618f3d5509930b940eaa3c49a3d0f
delgado7 <fabridelgado02@gmail.com>
2023-03-08 20:47:17 -0600

    Arreglo tags

--- Frontend/src/app/pages/gdd-setup-pages/game-genre/game-genre.component.ts
+++ Frontend/src/app/pages/gdd-setup-pages/game-genre/game-genre.component.ts
@@ -29,7 +29,7 @@ export class GameGenreComponent {
     this.getDataInJSONFormat();
   }
 
-  private deleteTags(id: number): void {
+  public deleteTag(id: number): void {
     // Delete at index 
     this.tags.splice(id, 1);
     console.log(this.tags);

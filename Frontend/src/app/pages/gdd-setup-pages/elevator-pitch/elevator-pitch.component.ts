05df1ba6a3b7d5bd70b24855c2f59b3a01157ee9
delgado7 <fabridelgado02@gmail.com>
2023-03-10 21:19:33 -0600

    Componente del elevator pitch unido con tema

--- Frontend/src/app/pages/gdd-setup-pages/elevator-pitch/elevator-pitch.component.ts
+++ Frontend/src/app/pages/gdd-setup-pages/elevator-pitch/elevator-pitch.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-elevator-pitch',
@@ -7,29 +8,49 @@ import { Component } from '@angular/core';
 })
 export class ElevatorPitchComponent {
   currentTextInBox: string;
+  subtitle: string;
+  routeUsingComponent: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.data.subscribe(_value => this.routeUsingComponent = _value.type);
+    this.subtitle = this.setSubtitle(this.routeUsingComponent);
     if (sessionStorage.getItem('currentSetup') !== null) {
-      let currentSetup = JSON.parse(sessionStorage.getItem('currentSetup'));
-      this.currentTextInBox = currentSetup.elevatorPitch;
+      this.getSessionStorageContent(this.routeUsingComponent);
     }
   }
 
   onTextChange(text: string) {
     this.currentTextInBox = text;
-    
-    let currentSetup = JSON.parse(sessionStorage.getItem('currentSetup'));
-    currentSetup.elevatorPitch = this.currentTextInBox;
-    sessionStorage.setItem('currentSetup', JSON.stringify(currentSetup));
+    this.setSessionStorageContent(this.routeUsingComponent);
   }
 
-  private getDataInJSONFormat(): Object {
-    let newJSON = { text: "" };
-    newJSON.text = this.currentTextInBox;
-    console.log(newJSON);
-    return newJSON;
+  private setSubtitle(route: string): string {
+    if (route === "elevator") {
+      return "Please write down the elevator pitch of your game";
+    } else if (route === "theme") {
+      return "Please write down the theme of your game";
+    } 
+  }
+
+  private getSessionStorageContent(route: string): void {
+    let currentSetup = JSON.parse(sessionStorage.getItem('currentSetup'));
+    if(route === "elevator") {
+      this.currentTextInBox = currentSetup.elevatorPitch;
+    } else if (route === "theme") {
+      this.currentTextInBox = currentSetup.theme;
+    }
+  }
+
+  private setSessionStorageContent(route: string): void {
+    let currentSetup = JSON.parse(sessionStorage.getItem('currentSetup'));
+    if (route === "elevator") {
+      currentSetup.elevatorPitch = this.currentTextInBox;
+    } else if (route === "theme") {
+      currentSetup.theme = this.currentTextInBox;
+    }
+    sessionStorage.setItem('currentSetup', JSON.stringify(currentSetup));
   }
 
 }

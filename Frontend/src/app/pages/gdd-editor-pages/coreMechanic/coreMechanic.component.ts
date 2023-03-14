ed7c3b1bc03509263a5cd851ac1de151189d7b43
Elias <eliasc20022002@gmail.com>
2023-03-14 14:44:14 -0600

    Object Updated

--- Frontend/src/app/pages/gdd-editor-pages/coreMechanic/coreMechanic.component.ts
+++ Frontend/src/app/pages/gdd-editor-pages/coreMechanic/coreMechanic.component.ts
@@ -11,35 +11,43 @@ export class CoreMechanicComponent {
   vditor2: Vditor | null = null;
   vditor3: Vditor | null = null;
   vditor4: Vditor | null = null;
-  coreMechanicContent: string[] = [
-    "El jugador se encuentra en una dimensión **desconocida** que no calza con todo lo real de la que debe escapar.",
-    "1. Middle\n\n2.Undeground",
-    "1. Full Entertainment for the player\n2. The player can interact with the world",
-    "# The Best Game Ever",
-  ];
+  coreMechanicContent = {
+    coreMechanic: "",
+    secondary: "",
+    progression: "",
+    metaphore: "",
+  };
 
   ngOnInit() {
     (this.vditor1 = new Vditor(
       "vditor1",
-      this.changeVditorConfig("Metaphore...", this.coreMechanicContent[0], 0)
+      this.changeVditorConfig(
+        "Metaphore...",
+        this.coreMechanicContent["metaphore"],
+        0
+      )
     )),
       (this.vditor2 = new Vditor(
         "vditor2",
         this.changeVditorConfig(
           "Progression...",
-          this.coreMechanicContent[1],
+          this.coreMechanicContent["progression"],
           1
         )
       ));
     this.vditor3 = new Vditor(
       "vditor3",
-      this.changeVditorConfig("Secondary...", this.coreMechanicContent[2], 2)
+      this.changeVditorConfig(
+        "Secondary...",
+        this.coreMechanicContent["secondary"],
+        2
+      )
     );
     this.vditor4 = new Vditor(
       "vditor4",
       this.changeVditorConfig(
         "Core Mechanic...",
-        this.coreMechanicContent[3],
+        this.coreMechanicContent["coreMechanic"],
         3
       )
     );

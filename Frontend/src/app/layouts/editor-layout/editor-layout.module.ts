625de41cc43454d17430430df2da08419e424e10
delgado7 <fabridelgado02@gmail.com>
2023-03-24 10:51:19 -0600

    Orden de directorios

--- Frontend/src/app/layouts/editor-layout/editor-layout.module.ts
+++ Frontend/src/app/layouts/editor-layout/editor-layout.module.ts
@@ -5,9 +5,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ClipboardModule } from 'ngx-clipboard';
 import { EditorLayoutRoutes } from './editor-layout.routing';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { VditorComponent } from "../../pages/gdd-editor-pages/vditor/vditor.component";
-import { AestheticsComponent } from 'src/app/pages/gdd-editor-pages/aesthetics/aesthetics.component';
-import { CoreMechanicComponent } from 'src/app/pages/gdd-editor-pages/coreMechanic/coreMechanic.component';
+import { VditorComponent } from "../../pages/gdd-editor-pages/high_level_design/vditor/vditor.component";
+import { AestheticsComponent } from 'src/app/pages/gdd-editor-pages/high_level_design/aesthetics/aesthetics.component';
+import { CoreMechanicComponent } from 'src/app/pages/gdd-editor-pages/high_level_design/coreMechanic/coreMechanic.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @NgModule({
--- Frontend/src/app/layouts/editor-layout/editor-layout.routing.ts
+++ Frontend/src/app/layouts/editor-layout/editor-layout.routing.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
-import { VditorComponent } from "../../pages/gdd-editor-pages/vditor/vditor.component";
-import { AestheticsComponent } from 'src/app/pages/gdd-editor-pages/aesthetics/aesthetics.component';
-import { CoreMechanicComponent } from 'src/app/pages/gdd-editor-pages/coreMechanic/coreMechanic.component';
+import { VditorComponent } from "../../pages/gdd-editor-pages/high_level_design/vditor/vditor.component";
+import { AestheticsComponent } from 'src/app/pages/gdd-editor-pages/high_level_design/aesthetics/aesthetics.component';
+import { CoreMechanicComponent } from 'src/app/pages/gdd-editor-pages/high_level_design/coreMechanic/coreMechanic.component';
 
 export const EditorLayoutRoutes: Routes = [
     { path: 'theme', component: VditorComponent, data: {section: "High Level Design", subSection: "Theme"} },

da7d65fb7e60da1947af33114f6673f35c2e454a
delgado7 <fabridelgado02@gmail.com>
2023-04-16 21:42:33 -0600

    Ajustes

--- Frontend/src/app/pages/register/register.component.ts
+++ Frontend/src/app/pages/register/register.component.ts
@@ -7,7 +7,7 @@ import { UserService } from "src/app/services/user.service";
 @Component({
   selector: "app-register",
   templateUrl: "./register.component.html",
-  styleUrls: ["./register.component.scss"],
+  styleUrls: ["./register.component.scss", "../login/login.component.scss"],
 })
 export class RegisterComponent implements OnInit {
   constructor(private http: HttpClient, private router: Router, private userService: UserService) { }
